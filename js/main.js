e01a7b51e75a9a2bd056d2a31434ce85b016f871
Seyi Samuel <seyi.samuel140@gmail.com>
2023-05-26 23:45:09 +0100

    Created a main.js file, then define and initialise some variables for DOM manipulations.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+    Author: Seyi Samuel
+    Description: Note app that allows user to create a note, edit it and also delete it with a local storage database facility.
+
+*/
+
+"use strict";
+
+/*  Variables*/
+//Get the dom element
+const newNoteBtn = document.querySelector(".btn");
+const noteForm = document.querySelector(".note-form");
+const noteContainer = document.querySelector(".note-container");
+const notesPanelDisplay = document.querySelector(".note-panel-display");
+
