0a18018ea3dcbfa9a9b318b71cd769a9d2c44d7a
Seyi Samuel <seyi.samuel140@gmail.com>
2023-05-26 23:56:46 +0100

    getNoteForm function and an event listener was added  to the file .

--- js/main.js
+++ js/main.js
@@ -13,3 +13,24 @@ const noteForm = document.querySelector(".note-form");
 const noteContainer = document.querySelector(".note-container");
 const notesPanelDisplay = document.querySelector(".note-panel-display");
 
+/*  Event Listeners*/
+newNoteBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("New note btn");
+    getNoteForm();
+
+});
+
+/* Functions */
+// note form display
+const getNoteForm = () => {
+    const noteForm = `
+    <div class="note-form">
+        <h3 id="note-heading">Add a new note</h3>
+        <input type="text" class="note-form-title" placeholder="note title">
+        <textarea placeholder="note description" class="note-form-description" cols="30" rows="10"></textarea>
+        <input  type="submit" value="add note">
+    </div>`;
+
+    noteContainer.insertAdjacentHTML("beforeend", noteForm);
+}
