504db10b95788f347e331f5857f8bb1cf687ad5e
Seyi Samuel <seyi.samuel140@gmail.com>
2023-05-27 23:30:21 +0100

    created the note display variable with set of elements in a div.

--- js/main.js
+++ js/main.js
@@ -13,6 +13,24 @@ const noteForm = document.querySelector(".note-form");
 const noteContainer = document.querySelector(".note-container");
 const notesPanelDisplay = document.querySelector(".note-panel-display");
 
+const noteDisplay = `
+    <div class="note">
+        <div class="note-text">
+            <h3 id="note-title">{ input.value }</h3>
+            <p id="note-description">{ textarea.value } </p>
+        </div>
+        <div class="note-addons">
+            <span class="date">{ new Date().getTime() }</span>
+            <span class="del-update">...
+                <ul>
+                    <button class="edit-note" type="button">update</button>
+                    <button class="delete-note" type="button">delete</button>
+                </ul>
+            </span>
+        </div>
+    </div>
+`;
+
 /*  Event Listeners*/
 newNoteBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -34,6 +52,8 @@ const getNoteForm = () => {
 
     noteContainer.insertAdjacentHTML("beforeend", noteForm);
 }
+
+
 
 // Display all notes
 const addNewNote = (inpVal, textVal, dateVal) => {
