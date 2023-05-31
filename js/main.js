f46a4d035b4ae01c14feae6545221dd4d6c21b85
Seyi Samuel <seyi.samuel140@gmail.com>
2023-05-31 17:25:07 +0100

    removed the getNotes function because I wanted to refactor my code to use classlist method

--- js/main.js
+++ js/main.js
@@ -35,23 +35,12 @@ const noteDisplay = `
 newNoteBtn.addEventListener("click", (e) => {
     e.preventDefault();
     console.log("New note btn");
-    getNoteForm();
+    
 
 });
 
 /* Functions */
-// note form display
-const getNoteForm = () => {
-    const noteForm = `
-    <div class="note-form">
-        <h3 id="note-heading">Add a new note</h3>
-        <input type="text" class="note-form-title" placeholder="note title">
-        <textarea placeholder="note description" class="note-form-description" cols="30" rows="10"></textarea>
-        <input  type="submit" value="add note">
-    </div>`;
 
-    noteContainer.insertAdjacentHTML("beforeend", noteForm);
-}
 
 
 
